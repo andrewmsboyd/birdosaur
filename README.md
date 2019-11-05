c0e00126343da1cde0d8adb21ce949a93c89ae95
Andrew M. S. Boyd <amsb@pm.me>
2019-11-05 14:06:53 -0600

    Added the while lood for iterating over each node and it's contained text and performing the replacement. Tested on the wiki page for Bird -- mostly successful, but now need to refin the RegEx to account for variations on capitalization, etc.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A browser extension created as a fun side project to replace all instances of the word **bird** on a webpage with the word **dinosaur**. Hilarious, right?
+
+Special shoutout to StackOverflow users zetlen and wOxxOm who initially taught me about TreeWalker.
--- birdosaur.js
+++ birdosaur.js
@@ -1,4 +1,13 @@
 // set a variable containing TreeWalker for all text nodes
 var textNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT),
-  birdRegEx = /bird/g
-  replaceWith = "dinosaur";
+    iterNode,
+    iterText,
+    birdRegEx = /bird/g
+    replaceWith = "dinosaur";
+
+  //iterate over each text node and make the replacement
+  while (textNodes.nextNode()) {
+    iterNode = textNodes.currentNode;
+    iterText = iterNode.nodeValue;
+    iterNode.nodeValue = iterText.replace(birdRegEx, replaceWith);
+  }
