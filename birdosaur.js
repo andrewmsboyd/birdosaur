211126c96d62bf99d4db1599f922f03e2694c7dd
Andrew M. S. Boyd <amsb@pm.me>
2019-11-05 13:02:44 -0600

    modified the method used to read page text into the 'pageContent' variable

--- birdosaur.js
+++ birdosaur.js
@@ -1,5 +1,5 @@
 // read webpage text into a variable
-var pageContent = document.getElementById("content");
+var pageContent = document.body.innerText;
 
 // set the regex string to be replaced within the page
 var birdosaur = new RegExp(/bird/gi);
