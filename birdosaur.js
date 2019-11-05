8ed4bb345b55e276ea6fd4d7d2582d031eccaa19
Andrew M. S. Boyd <amsb@pm.me>
2019-11-05 13:38:17 -0600

    changed the script to the very bad and very messy and very simple method of just replacing the innterText on the page. This is just placeholder -- working on the better solution.

--- birdosaur.js
+++ birdosaur.js
@@ -1,8 +1,8 @@
 // read webpage text into a variable
-var pageContent = document.body.innerText;
+//var pageContent = document.body.innerText;
 
 // set the regex string to be replaced within the page
-var birdosaur = new RegExp(/bird/gi);
+//var birdosaur = new RegExp(/bird/gi);
 
 
-pageContent.replace(birdosaur, 'dinosaur');
+document.body.innerText = document.body.innerText.replace(/bird/g, 'dinosaur');
